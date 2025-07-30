c56af99146d93602e12b17cc4f7a5f27bacbd93e
Ukyo Matsuzaki <ag.hg2pu.au@gmail.com>
2025-07-31 01:47:47 +0900

    初回コミット

---
+++ designsystem/src/main/kotlin/com/yourorg/designsystem/Color.kt
@@ -0,0 +1,5 @@
+object KukuColor {
+    val Surface = Color(0xFFD9D9D9)
+    val Accent  = Color(0xFFFF9CA1)
+    val Bg      = Color.White
+}
---
+++ designsystem/src/main/kotlin/com/yourorg/designsystem/Typography.kt
@@ -0,0 +1,4 @@
+val KukuTypography = Typography(
+    headlineLarge = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold),
+    bodyLarge     = TextStyle(fontSize = 16.sp)
+)
